de04e8ee697fb758b8142c682e3e6bed2d36789f
YMB01 <25132dev@gmail.com>
2024-04-21 12:47:36 +0300

    remove comment

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -34,8 +34,4 @@ export class InMemoryDataService implements InMemoryDbService {
 }
 
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
+ 
